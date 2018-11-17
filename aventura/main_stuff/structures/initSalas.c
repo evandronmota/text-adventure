163c61e307f4eda2e3414575efe8f2e4bcf07089
leonardo <leonardo@lap.derp>
2018-11-16 22:50:19 -0200

    kanban atualizado

--- aventura/main_stuff/structures/initSalas.c
+++ aventura/main_stuff/structures/initSalas.c
@@ -16,27 +16,34 @@ Elemento *inicializarSalas() {
     Elemento sala4 = criarSala4();
     Elemento sala5 = criarSala5();
 
-    sala1.detalhe.saidas = malloc(sizeof(Elemento*));
-    sala1.detalhe.saidas[0] = &lobby;
-
-    sala2.detalhe.saidas = malloc(sizeof(Elemento*));
-    sala2.detalhe.saidas[0] = &lobby;
-
-    sala3.detalhe.saidas = malloc(sizeof(Elemento*));
-    sala3.detalhe.saidas[0] = &lobby;
-
-    sala4.detalhe.saidas = malloc(sizeof(Elemento*));
-    sala4.detalhe.saidas[0] = &lobby;
-
-    sala5.detalhe.saidas = malloc(sizeof(Elemento*));
-    sala5.detalhe.saidas[0] = &lobby;
-
     lobby.detalhe.saidas = malloc(5*sizeof(Elemento*));
-    lobby.detalhe.saidas[0] = &sala1;
-    lobby.detalhe.saidas[1] = &sala2;
-    lobby.detalhe.saidas[2] = &sala3;
-    lobby.detalhe.saidas[3] = &sala4;
-    lobby.detalhe.saidas[4] = &sala5;
+    sala1.detalhe.saidas = malloc(sizeof(Elemento*));
+    sala2.detalhe.saidas = malloc(sizeof(Elemento*));
+    sala3.detalhe.saidas = malloc(sizeof(Elemento*));
+    sala4.detalhe.saidas = malloc(sizeof(Elemento*));
+    sala5.detalhe.saidas = malloc(sizeof(Elemento*));
+
+    // direções cardeais
+    for (int i = 0; i < 4; i++) {
+        lobby.detalhe.saidas[i] = NULL;
+        sala1.detalhe.saidas[i] = NULL;
+        sala2.detalhe.saidas[i] = NULL;
+        sala3.detalhe.saidas[i] = NULL;
+        sala4.detalhe.saidas[i] = NULL;
+        sala5.detalhe.saidas[i] = NULL;
+    }
+
+    sala1.detalhe.saidas[4] = &lobby;
+    sala2.detalhe.saidas[4] = &lobby;
+    sala3.detalhe.saidas[4] = &lobby;
+    sala4.detalhe.saidas[4] = &lobby;
+    sala5.detalhe.saidas[4] = &lobby;
+
+    lobby.detalhe.saidas[4] = &sala1;
+    lobby.detalhe.saidas[5] = &sala2;
+    lobby.detalhe.saidas[6] = &sala3;
+    lobby.detalhe.saidas[7] = &sala4;
+    lobby.detalhe.saidas[8] = &sala5;
 
     Elemento *salas = malloc(5*sizeof(Elemento));
     salas[0] = lobby;
