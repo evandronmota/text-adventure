7ce562b0483a0e65d990afc36d3471963dac2d23
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2018-11-17 22:54:57 -0200

    Funcinando

--- aventura/main_stuff/structures/salas.c
+++ aventura/main_stuff/structures/salas.c
@@ -56,27 +56,6 @@ int colocarSobre(Elemento *e1, Elemento *e2) {
 }
 
 /*
-    Recebe dois ponteiros para elementos.
-    Imprime uma mensagem avisando que pegou o primeiro elemento.
-    Retorna um inteiro indicando se a ação foi
-    bem-sucedida.
-*/
-int pegar(Elemento *e1, Elemento *e2) {
-    if (e1 == NULL)
-        printf("Pegar o quê?\n");
-    // else {
-    //     if (!buscarnaMochila(e1)) {
-    //         printf("Você pegou %s %s.", e1->artigo, e1->n);
-    //         adicionarnaMochila(e1);
-    //     }
-    //     else printf("Você já pegou %s %s.", e1->artigo, e1->n);
-
-    // }
-
-    return (e1 == NULL ? 0 : 1);
-}
-
-/*
     Recebe os parâmetros necessários ao elemento.
     Cria um elemento com esses parâmetros.
     Retorna o elemento criado.
