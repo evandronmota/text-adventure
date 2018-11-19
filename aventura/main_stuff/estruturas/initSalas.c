34cf8ada807eae28166d58b8a7ca01eadc101d3f
Evandro Nakayama Mota <evandro.nmota5@gmail.com>
2018-11-18 23:36:37 -0200

    fix saidas?

--- aventura/main_stuff/estruturas/initSalas.c
+++ aventura/main_stuff/estruturas/initSalas.c
@@ -12,51 +12,79 @@ Elemento *inicializarSalas() {
     Elemento sala4 = criarSala4();
     Elemento sala5 = criarSala5();
 
-    // sala1.detalhe.saidas = malloc(5 * sizeof(saidas*));
-    // sala2.detalhe.saidas = malloc(5 * sizeof(saidas*));
-    // sala3.detalhe.saidas = malloc(5 * sizeof(saidas*));
-    // sala4.detalhe.saidas = malloc(5 * sizeof(saidas*));
-    // sala5.detalhe.saidas = malloc(5 * sizeof(saidas*));
-    // lobby.detalhe.saidas = malloc(9 * sizeof(saidas*));
-    // exit(0);
+    sala1.detalhe.saidas = malloc(5 * sizeof(saidas*));
+    sala2.detalhe.saidas = malloc(5 * sizeof(saidas*));
+    sala3.detalhe.saidas = malloc(5 * sizeof(saidas*));
+    sala4.detalhe.saidas = malloc(5 * sizeof(saidas*));
+    sala5.detalhe.saidas = malloc(5 * sizeof(saidas*));
+    lobby.detalhe.saidas = malloc(9 * sizeof(saidas*));
 
-    // /* Direções cardeais */
-    // for (int i = 0; i < 4; i++) {
-    //     // sala1.detalhe.saidas[i]->saida = NULL;
-    //     sala1.detalhe.saidas[i]->esta_fechada = True;
-    //     // sala2.detalhe.saidas[i]->saida = NULL;
-    //     sala2.detalhe.saidas[i]->esta_fechada = True;
-    //     // sala3.detalhe.saidas[i]->saida = NULL;
-    //     sala3.detalhe.saidas[i]->esta_fechada = True;
-    //     // sala4.detalhe.saidas[i]->saida = NULL;
-    //     sala4.detalhe.saidas[i]->esta_fechada = True;
-    //     // sala5.detalhe.saidas[i]->saida = NULL;
-    //     sala5.detalhe.saidas[i]->esta_fechada = True;
-    //     // lobby.detalhe.saidas[i]->saida = NULL;
-    //     lobby.detalhe.saidas[i]->esta_fechada = True;
-    // }
+    /* Direções cardeais */
+    for (int i = 0; i < 4; i++) {
+        sala1.detalhe.saidas[i] = malloc(sizeof(saidas));
+        sala1.detalhe.saidas[i]->saida = NULL;
+        sala1.detalhe.saidas[i]->esta_fechada = True;
 
-    // sala1.detalhe.saidas[4]->saida = &lobby;
-    // sala1.detalhe.saidas[4]->esta_fechada = False;
-    // sala2.detalhe.saidas[4]->saida = &lobby;
-    // sala2.detalhe.saidas[4]->esta_fechada = False;
-    // sala3.detalhe.saidas[4]->saida = &lobby;
-    // sala3.detalhe.saidas[4]->esta_fechada = False;
-    // sala4.detalhe.saidas[4]->saida = &lobby;
-    // sala4.detalhe.saidas[4]->esta_fechada = False;
-    // sala5.detalhe.saidas[4]->saida = &lobby;
-    // sala5.detalhe.saidas[4]->esta_fechada = False;
+        sala2.detalhe.saidas[i] = malloc(sizeof(saidas));
+        sala2.detalhe.saidas[i]->saida = NULL;
+        sala2.detalhe.saidas[i]->esta_fechada = True;
 
-    // lobby.detalhe.saidas[4]->saida = &sala1;
-    // lobby.detalhe.saidas[4]->esta_fechada = False;
-    // lobby.detalhe.saidas[5]->saida = &sala2;
-    // lobby.detalhe.saidas[5]->esta_fechada = False;
-    // lobby.detalhe.saidas[6]->saida = &sala3;
-    // lobby.detalhe.saidas[6]->esta_fechada = False;
-    // lobby.detalhe.saidas[7]->saida = &sala4;
-    // lobby.detalhe.saidas[7]->esta_fechada = False;
-    // lobby.detalhe.saidas[8]->saida = &sala5;
-    // lobby.detalhe.saidas[8]->esta_fechada = False;
+        sala3.detalhe.saidas[i] = malloc(sizeof(saidas));
+        sala3.detalhe.saidas[i]->saida = NULL;
+        sala3.detalhe.saidas[i]->esta_fechada = True;
+
+        sala4.detalhe.saidas[i] = malloc(sizeof(saidas));
+        sala4.detalhe.saidas[i]->saida = NULL;
+        sala4.detalhe.saidas[i]->esta_fechada = True;
+
+        sala5.detalhe.saidas[i] = malloc(sizeof(saidas));
+        sala5.detalhe.saidas[i]->saida = NULL;
+        sala5.detalhe.saidas[i]->esta_fechada = True;
+
+        lobby.detalhe.saidas[i] = malloc(sizeof(saidas));
+        lobby.detalhe.saidas[i]->saida = NULL;
+        lobby.detalhe.saidas[i]->esta_fechada = True;
+    }
+
+    sala1.detalhe.saidas[4] = malloc(sizeof(saidas));
+    sala1.detalhe.saidas[4]->saida = &lobby;
+    sala1.detalhe.saidas[4]->esta_fechada = False;
+
+    sala2.detalhe.saidas[4] = malloc(sizeof(saidas));
+    sala2.detalhe.saidas[4]->saida = &lobby;
+    sala2.detalhe.saidas[4]->esta_fechada = False;
+
+    sala3.detalhe.saidas[4] = malloc(sizeof(saidas));
+    sala3.detalhe.saidas[4]->saida = &lobby;
+    sala3.detalhe.saidas[4]->esta_fechada = False;
+
+    sala4.detalhe.saidas[4] = malloc(sizeof(saidas));
+    sala4.detalhe.saidas[4]->saida = &lobby;
+    sala4.detalhe.saidas[4]->esta_fechada = False;
+
+    sala5.detalhe.saidas[4] = malloc(sizeof(saidas));
+    sala5.detalhe.saidas[4]->saida = &lobby;
+    sala5.detalhe.saidas[4]->esta_fechada = False;
+
+    lobby.detalhe.saidas[4] = malloc(sizeof(saidas));
+    lobby.detalhe.saidas[4]->saida = &sala1;
+    lobby.detalhe.saidas[4]->esta_fechada = False;
+
+    lobby.detalhe.saidas[5] = malloc(sizeof(saidas));
+    lobby.detalhe.saidas[5]->saida = &sala2;
+    lobby.detalhe.saidas[5]->esta_fechada = False;
+
+    lobby.detalhe.saidas[6] = malloc(sizeof(saidas));
+    lobby.detalhe.saidas[6]->saida = &sala3;
+    lobby.detalhe.saidas[6]->esta_fechada = False;
+
+    lobby.detalhe.saidas[7] = malloc(sizeof(saidas));
+    lobby.detalhe.saidas[7]->saida = &sala4;
+    lobby.detalhe.saidas[7]->esta_fechada = False;
+
+    lobby.detalhe.saidas[8] = malloc(sizeof(saidas));
+    lobby.detalhe.saidas[8]->saida = &sala5;
+    lobby.detalhe.saidas[8]->esta_fechada = False;
 
     Elemento *salas = malloc(6 * sizeof(Elemento));
 
